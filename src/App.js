d6904228678aa7344e325a7cb9fb6aa7dd273333
Donard97 <donardgolaj@gmail.com>
2022-03-03 23:12:21 +0100

    Create files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeContainer from './containers/HomeContainer';
 
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomeContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomeContainer = () => (
+  <div>Hello</div>
+);
+
+export default HomeContainer;
