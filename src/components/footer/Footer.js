f94de7ec0ce61254fa589871c40776b07c207bd3
Donard97 <donardgolaj@gmail.com>
2022-03-16 00:12:34 +0100

    Add icons to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import '../../stylesheet/footer.css';
 
-const Footer = () => {
+function Footer() {
   return (
-    <div>Footer</div>
+    <Box className="footer">
+      <ul className="footerList">
+        <li>
+          <Typography variant="p" component="div">find me by:</Typography>
+        </li>
+        <li>
+          <LinkedInIcon />
+        </li>
+        <li>
+          <GitHubIcon />
+        </li>
+        <li>
+          <TwitterIcon />
+        </li>
+      </ul>
+    </Box>
   );
-};
+}
 
 export default Footer;
