9905c3f70532ba039d00a72890bf77b42a3ff409
Donard97 <donardgolaj@gmail.com>
2022-03-16 11:51:55 +0100

    Deploy project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
-import AboutContainer from './containers/AboutContainer';
-import ProjectsContainer from './containers/ProjectsContainer';
-import ContactContainer from './containers/ContactContainer';
+// import AboutContainer from './containers/AboutContainer';
+// import ProjectsContainer from './containers/ProjectsContainer';
+// import ContactContainer from './containers/ContactContainer';
 
 function App() {
   return (
@@ -11,9 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomeContainer />} />
-          <Route exact path="/about" element={<AboutContainer />} />
+          {/* <Route exact path="/about" element={<AboutContainer />} />
           <Route exact path="/projects" element={<ProjectsContainer />} />
-          <Route exact path="/contact" element={<ContactContainer />} />
+          <Route exact path="/contact" element={<ContactContainer />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
