fe5f96e8676f41352af0807c72acc9fe10f839b3
Donard97 <donardgolaj@gmail.com>
2022-03-20 01:48:11 +0100

    Add paragraph on about me page

---
+++ src/components/about/About.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import '../../stylesheet/about.css';
+
+function About() {
+  return (
+    <Container className="aboutContainer">
+      <div className="numberList">
+        <ul>
+          <li>24</li>
+          <li>25</li>
+          <li>26</li>
+          <li>27</li>
+          <li>28</li>
+          <li>29</li>
+          <li>30</li>
+          <li>31</li>
+          <li>32</li>
+          <li>33</li>
+          <li>34</li>
+          <li>35</li>
+          <li>36</li>
+          <li>37</li>
+          <li>38</li>
+          <li>39</li>
+          <li>41</li>
+          <li>42</li>
+          <li>43</li>
+          <li>44</li>
+          <li>45</li>
+          <li>46</li>
+          <li>47</li>
+          <li>48</li>
+          <li>49</li>
+          <li>50</li>
+        </ul>
+      </div>
+      <div className="aboutParagraph">
+        <p>
+          About me:
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.
+          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.
+        </p>
+      </div>
+    </Container>
+  );
+}
+
+export default About;
--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/nav_bar/NavBar';
 import Footer from '../components/footer/Footer';
 import Skills from '../components/skills/Skills';
+import About from '../components/about/About';
 
 function AboutContainer() {
   return (
     <>
       <Navbar />
       <Skills />
+      <About />
       <Footer />
     </>
   );
